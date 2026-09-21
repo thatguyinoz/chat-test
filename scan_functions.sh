4bd185fe2b63fcc039f4ecce8c22484f039f2348
Your Name <you@example.com>
2026-09-21 15:09:37 +0800

    Initial commit

--- scan_functions.sh
+++
@@ -1,5 +0,0 @@
-# Function to perform the SSH scan
-perform_ssh_scan() {
-    echo "Scanning network $NETWORK_RANGE for SSH servers on ports $PORTS..."
-    nmap -p $PORTS --open $NETWORK_RANGE | grep -E "open\s+ssh" | awk '{print $1, $2, $3}'
-}
